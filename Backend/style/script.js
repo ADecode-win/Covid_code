30eb235011fb91eb7014eab70a310d363f66fcc2
Adwitiya Argha Priyadarshini Boruah <80182151+ADecode-win@users.noreply.github.com>
2024-05-07 09:18:05 +0000

    Changes made to html, js

--- Backend/style/script.js
+++ Backend/style/script.js
@@ -3,11 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const width = 1000 - margin.left - margin.right;
     const height = 800 - margin.top - margin.bottom;
     const parseDate = d3.timeParse("%d/%m/%Y");
-    const formatDate = d3.timeFormat("%d.%m.%Y"); // Changed to required format
+    const formatDate = d3.timeFormat("%d.%m.%Y");
+    const year = 2020;
 
-    const europeanCountries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia_and_Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 
-        'Denmark', 'Estonia', 'Faroe_Islands', 'Finland', 'France', 'Georgia', 'Germany', 'Gibraltar', 'Greece', 'Guernsey', 'Holy_See', 'Hungary', 
-        'Iceland', 'Ireland', 'Isle_of_Man', 'Italy', 'Jersey', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 
+    const europeanCountries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia_and_Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia',
+        'Denmark', 'Estonia', 'Faroe_Islands', 'Finland', 'France', 'Georgia', 'Germany', 'Gibraltar', 'Greece', 'Guernsey', 'Holy_See', 'Hungary',
+        'Iceland', 'Ireland', 'Isle_of_Man', 'Italy', 'Jersey', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova',
         'Monaco', 'Montenegro', 'Netherlands', 'North_Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San_Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey', 'Ukraine', 'United_Kingdom'];
 
     const selector = d3.select("#country-selector");
@@ -15,14 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
         .data(europeanCountries)
         .enter().append("option")
         .attr("value", d => d)
-        .text(d => d)
-        .text(d => d ? d.replace(/_/g, ' ') : 'Select a country');
-      
-    const messageContainer = d3.select("#message-container");
-    const focusLink = document.getElementById("focus-link");
-    const svgContainer = d3.select("#chart-container");
+        .text(d => d.replace(/_/g, ' '));
 
-    const svg = d3.select("#chart-container").append("svg")
+    const monthSelector = d3.select("#month-selector");
+    const svgContainer = d3.select("#chart-container");
+    const svg = svgContainer.append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -36,129 +34,191 @@ document.addEventListener("DOMContentLoaded", function() {
         .style("border", "1px solid #ccc")
         .style("padding", "5px");
 
-    let circlesVisible = false;  // NEW: State to track visibility of circles
+    const messageContainer = d3.select("#message-container");
+    const focusLink = document.getElementById("focus-link");
 
-    const toggleButton = d3.select("#toggle-btn");  // NEW: Select the toggle button
-    toggleButton.on("click", function() {  // NEW: Add click event listener
-            circlesVisible = !circlesVisible;  // NEW: Toggle the visibility state
-            svg.selectAll("circle").style("display", circlesVisible ? null : "none");  // NEW: Apply visibility
-            this.textContent = circlesVisible ? "Hide Data Points" : "Show Data Points";  // NEW: Update button text
-        });
-    
+    let circlesVisible = true;
+    let timer, playing = false;
+
+    const toggleButton = d3.select("#toggle-btn");
+    toggleButton.on("click", function() {
+        circlesVisible = !circlesVisible;
+        svg.selectAll("circle").style("display", circlesVisible ? null : "none");
+        this.textContent = circlesVisible ? "Hide Data Points" : "Show Data Points";
+    });
+
+    const downloadButton = document.getElementById("download-btn");
+    downloadButton.addEventListener("click", function() {
+        const fileUrl = "/fhir_bundle.json";
+        const downloadLink = document.createElement("a");
+        downloadLink.href = fileUrl;
+        downloadLink.download = "fhir_bundle.json";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
 
     d3.json("filtered_european_data.json").then(function(data) {
-      data.forEach(d => {
-        d.date = parseDate(d.dateRep);
-        d.cases = +d.cases;
-        d.deaths = +d.deaths;
-      });
+        data.forEach(d => {
+            d.date = parseDate(d.dateRep);
+            d.cases = +d.cases;
+            d.deaths = +d.deaths;
+        });
 
-      let timer, playing = false;
-      const dates = data.map(d => d.date);
-      const uniqueDates = [...new Set(dates)].sort(d3.ascending);
+        const dates = data.map(d => d.date);
+        const uniqueDates = [...new Set(dates)].sort(d3.ascending);
 
-      const updateChart = function(date) {
-        const country = selector.property('value');
-        const filteredData = data.filter(d => d.date <= date && d.countriesAndTerritories === country);
-        const x = d3.scaleTime().range([0, width]).domain(d3.extent(filteredData, d => d.date));
-        const y = d3.scaleLinear().range([height, 0]).domain([0, d3.max(filteredData, d => d.cases)]);
-        svg.selectAll("*").remove(); // Clear previous SVG contents
+        function filterDataByMonthAndCountry(country, month) {
+            let filteredData;
+            if (month === "None") {
+                filteredData = data.filter(d => d.countriesAndTerritories === country);
+            } else {
+                const startDate = new Date(`${year}-${month}-01`);
+                const endDate = new Date(startDate);
+                endDate.setMonth(endDate.getMonth() + 1);
+                endDate.setDate(0);
+                filteredData = data.filter(d => d.countriesAndTerritories === country && d.date >= startDate && d.date <= endDate);
+            }
+            return filteredData;
+        }
 
-        svg.append("g")
-            .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(x).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat("%B %Y")))
-            .selectAll("text")
-            .attr("transform", "rotate(-45)")
-            .style("text-anchor", "end");
+        function updateChartTitle(country, month) {
+            const monthFormatter = new Intl.DateTimeFormat('en-US', { month: 'long' });
+            let title = `Total Number of Covid cases in ${country.replace(/_/g, ' ')}`;
+            if (month === "None") {
+                title += " in 2020";
+            } else {
+                const monthDate = new Date(`${year}-${month}-01`);
+                const monthName = monthFormatter.format(monthDate);
+                title += ` in ${monthName} ${year}`;
+            }
+            return title;
+        }
 
-        svg.append("g").call(d3.axisLeft(y));
+        function updateChart(country, date = null) {
+            const month = monthSelector.node().value;
+            const filteredData = filterDataByMonthAndCountry(country, month);
 
-        const line = d3.line().x(d => x(d.date)).y(d => y(d.cases));
-        svg.append("path")
-            .datum(filteredData)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 2)
-            .attr("d", line);
-      
-            // Circles for data points
-            svg.selectAll("circle")  // NEW: Modified to create circles conditionally based on circlesVisible
-            .data(filteredData)
-            .enter()
-            .append("circle")
-            .attr("cx", d => x(d.date))
-            .attr("cy", d => y(d.cases))
-            .attr("r", 5)
-            .attr("fill", "red")
-            .style("display", circlesVisible ? null : "none")  // NEW: Apply initial visibility state
-            .on("mouseover", function(event, d) {
-                tooltip.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                tooltip.html("Date: " + formatDate(d.date) + "<br/>Cases: " + d.cases + "<br/>Deaths: " + d.deaths)
-                    .style("left", (event.pageX) + "px")
-                    .style("top", (event.pageY - 28) + "px");
-            })
-            .on("mouseout", function(d) {
-                tooltip.transition()
-                    .duration(500)
-                    .style("opacity", 0);
+            const xDomain = date ? [d3.min(filteredData, d => d.date), date] : d3.extent(filteredData, d => d.date);
+            const x = d3.scaleTime().range([0, width]).domain(xDomain);
+            const y = d3.scaleLinear().range([height, 0]).domain([0, d3.max(filteredData, d => d.cases)]);
+
+            svg.selectAll("*").remove();
+
+            svg.append("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d.%m.%Y")))
+                .selectAll("text")
+                .attr("transform", "rotate(-45)")
+                .style("text-anchor", "end");
+
+            svg.append("g").call(d3.axisLeft(y));
+
+            const line = d3.line().x(d => x(d.date)).y(d => y(d.cases));
+            svg.append("path")
+                .datum(filteredData.filter(d => !date || d.date <= date))
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-width", 2)
+                .attr("d", line);
+
+            svg.selectAll("circle")
+                .data(filteredData.filter(d => !date || d.date <= date))
+                .enter()
+                .append("circle")
+                .attr("cx", d => x(d.date))
+                .attr("cy", d => y(d.cases))
+                .attr("r", 5)
+                .attr("fill", "red")
+                .style("display", circlesVisible ? null : "none")
+                .on("mouseover", function(event, d) {
+                    tooltip.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    tooltip.html("Date: " + formatDate(d.date) + "<br/>Cases: " + d.cases + "<br/>Deaths: " + d.deaths)
+                        .style("left", (event.pageX) + "px")
+                        .style("top", (event.pageY - 28) + "px");
+                })
+                .on("mouseout", function() {
+                    tooltip.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                });
+
+            svg.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 0 - (margin.left - 20))
+                .attr("x", 0 - (height / 2))
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .style("font-size", "14px")
+                .style("fill", "#777")
+                .style("font-family", "sans-serif")
+                .text("Total Number of Covid cases");
+
+            svg.append("text")
+                .attr("class", "chart-title")
+                .attr("x", margin.left - 50)
+                .attr("y", margin.top - 420)
+                .style("font-size", "24px")
+                .style("font-weight", "bold")
+                .style("font-family", "sans-serif")
+                .text(updateChartTitle(country, month));
+        }
+
+        function updateUIBasedOnSelection(value) {
+            const month = monthSelector.node().value;
+            if (value) {
+                if (month === "None") {
+                    sliderSvg.style("display", "block");
+                    playButton.style("display", "block");
+                } else {
+                    sliderSvg.style("display", "none");
+                    playButton.style("display", "none");
+                }
+                messageContainer.style("visibility", "hidden");
+                svgContainer.style("display", "block");
+                updateChart(value, month === "None" ? slider.value() : null);
+            } else {
+                sliderSvg.style("display", "none");
+                playButton.style("display", "none");
+                messageContainer.style("visibility", "visible");
+                svgContainer.style("display", "none");
+            }
+        }
+
+        const sliderSvg = d3.select('#slider-range').append('svg')
+            .attr('width', 920)
+            .attr('height', 100)
+            .style("display", "none");
+
+        const sliderG = sliderSvg.append('g')
+            .attr('transform', 'translate(50,30)');
+
+        const slider = d3.sliderBottom()
+            .min(d3.min(uniqueDates))
+            .max(d3.max(uniqueDates))
+            .step(1000 * 60 * 60 * 24)
+            .width(800)
+            .tickFormat(d3.timeFormat("%d %b %Y"))
+            .ticks(5)
+            .fill('#008000')
+            .on('onchange', val => {
+                updateChart(selector.node().value, val);
             });
 
-        svg.append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 0 - (margin.left - 20))
-          .attr("x", 0 - (height / 2))
-          .attr("dy", "1em")
-          .style("text-anchor", "middle")
-          .style("font-size", "14px")
-          .style("fill", "#777")
-          .style("font-family", "sans-serif")
-          .text("Total Number of Covid cases");
+        sliderG.call(slider);
 
-        svg.append("text")
-          .attr("class", "chart-title")
-          .attr("x", margin.left - 50)
-          .attr("y", margin.top - 420)
-          .style("font-size", "24px")
-          .style("font-weight", "bold")
-          .style("font-family", "sans-serif")
-          .text("Total Number of Covid cases in " + country + " in the year 2020");
-      };
-
-  const sliderSvg = d3.select('#slider-range').append('svg')
-      .attr('width', 920) // Increased width to accommodate button
-      .attr('height', 100)
-      .style("display", "none");
-
-  const sliderG = sliderSvg.append('g')
-      .attr('transform', 'translate(50,30)');
-
-  const slider = d3.sliderBottom()
-      .min(d3.min(uniqueDates))
-      .max(d3.max(uniqueDates))
-      .step(1000 * 60 * 60 * 24) // one day
-      .width(800)
-      .tickFormat(d3.timeFormat("%d %b %Y"))
-      .ticks(5)
-      .fill('#008000')
-      .on('onchange', val => {
-        updateChart(val);
-      });
-
-  sliderG.call(slider);
-
-  // Adding play/pause button next to the slider
-  const playButton = sliderSvg.append('foreignObject')
-      .attr('x', 860)
-      .attr('y', 20)
-      .attr('width', 120)
-      .attr('height', 50)
-      .append("xhtml:button")
-      .attr('class', 'play-pause-btn')
-      .text('Play')
-      .style("display", "none")
-      .style("background-color", "#32CD32") // Initial color is green
+        const playButton = sliderSvg.append('foreignObject')
+            .attr('x', 860)
+            .attr('y', 20)
+            .attr('width', 120)
+            .attr('height', 50)
+            .append("xhtml:button")
+            .attr('class', 'play-pause-btn')
+            .text('Play')
+            .style("display", "block")
+            .style("background-color", "#32CD32")
             .on('click', function() {
                 if (!playing) {
                     timer = setInterval(function() {
@@ -167,62 +227,43 @@ document.addEventListener("DOMContentLoaded", function() {
                         if (nextDate > d3.max(uniqueDates)) {
                             clearInterval(timer);
                             playing = false;
-                            this.textContent = 'Play';
-                            this.style.backgroundColor = '#32CD32'; // Set to green when paused
-                            d3.selectAll("circle").attr("pointer-events", "all"); // Enable mouse events on circles
+                            playButton.textContent = 'Play';
+                            playButton.style.backgroundColor = '#32CD32';
                         } else {
                             slider.value(nextDate);
-                            updateChart(nextDate);
+                            updateChart(selector.node().value, nextDate);
                         }
-                    }, 100); // Adjust time for faster/slower playback
+                    }, 100);
                     playing = true;
                     this.textContent = 'Pause';
-                    this.style.backgroundColor = '#EE4B2B'; // Set to red when playing
-                    d3.selectAll("circle").attr("pointer-events", "none"); // Disable mouse events on circles during play
+                    this.style.backgroundColor = '#EE4B2B';
                 } else {
                     clearInterval(timer);
                     playing = false;
                     this.textContent = 'Play';
-                    this.style.backgroundColor = '#32CD32'; // Set to green when paused
-                    d3.selectAll("circle").attr("pointer-events", "all"); // Enable mouse events on circles after pause
+                    this.style.backgroundColor = '#32CD32';
                 }
             });
 
-    // Event listener for when the country selection changes
-    updateUIBasedOnSelection(selector.property('value'));
+        selector.on("change", function() {
+            updateUIBasedOnSelection(this.value);
+        });
 
-    // Update message visibility based on selection
-    selector.on("change", function() {
-      updateUIBasedOnSelection(this.value);
-    });
-    function updateUIBasedOnSelection(value) {
-        if (value) {
-            // If a country is selected, show the slider and play button, and hide the message
-            sliderSvg.style("display", "block");
-            playButton.style("display", "block");
-            messageContainer.style("visibility", "hidden");
-            svgContainer.style("display", "block");
-            updateChart(slider.value()); // Assuming you have a function to update your chart
-        } else {
-            // If no country is selected, hide the slider and play button, and show the message
-            sliderSvg.style("display", "none");
-            playButton.style("display", "none");
-            messageContainer.style("visibility", "visible");
-            svgContainer.style("display", "none");
-        }
-      }
-    
+        monthSelector.on("change", function() {
+            updateUIBasedOnSelection(selector.node().value);
+        });
+
         focusLink.addEventListener("click", function(event) {
-          event.preventDefault(); // Prevent the default anchor behavior
-          // Hide the message container when the link is clicked
-          messageContainer.style("visibility", "hidden");
-          svgContainer.style("display", "none");
-          // Focus on the dropdown to allow for immediate selection
-          const dropdown = document.getElementById("country-selector");
-          if (dropdown) {
-              dropdown.focus();
-          }
-          return false;
-      })
+            event.preventDefault();
+            messageContainer.style("visibility", "hidden");
+            svgContainer.style("display", "none");
+            const dropdown = document.getElementById("country-selector");
+            if (dropdown) {
+                dropdown.focus();
+            }
+            return false;
+        });
+
+        updateUIBasedOnSelection(selector.property('value'));
     });
- }); 
+});
